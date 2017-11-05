fea8bda68aed933b61d5fbd9233df970e57dcac7
Shareza <michal.soja90@gmail.com>
2017-11-05 17:17:56 +0100

    Initial commit with world, player and example enemy

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob : MonoBehaviour {
+
+
+    public float speed;
+    public float range;
+    public CharacterController controller;
+    public Transform player;
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Chase();	
+	}
+
+    bool InRange()
+    {
+        if(Vector3.Distance(transform.position, player.position) < range)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Chase()
+    {
+        transform.LookAt(player.position);
+        controller.SimpleMove(transform.forward * speed);
+        animator.SetBool("IsWalking", true);
+
+    }
+}
