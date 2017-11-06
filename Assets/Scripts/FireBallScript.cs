cb8ac3ee006c843fa613eed54c340353625f178a
Shareza <michal.soja90@gmail.com>
2017-11-06 21:15:07 +0100

    Better Fireball texture, added Fireball life time

---
+++ Assets/Scripts/FireBallScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallScript : MonoBehaviour {
+
+    Rigidbody rb;
+    public float speed = 1.5f;
+    public float lifeTime = 0.01f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+
+    private void Update()
+    {
+        DestroyFireBallAfterLifeTimeFades();
+    }
+
+    private void DestroyFireBallAfterLifeTimeFades()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
---
+++ Assets/Scripts/SpellsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellsController : MonoBehaviour {
+
+    public GameObject fireBall;
+    public Transform fireBallSpawn;
+    public float fireRate;
+    public float nextFire;
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && Time.time > nextFire)
+        {
+            nextFire = Time.time * fireRate;
+            GameObject clone = Instantiate(fireBall, fireBallSpawn.position, fireBallSpawn.rotation) as GameObject;
+        }
+    }
+}
