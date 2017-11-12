c58991877ffd76ed1798808fa625c140e0744df9
Shareza <michal.soja90@gmail.com>
2017-11-12 09:49:07 +0100

    Quick commit

---
+++ Assets/SkeletonHealth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkeletonHealth : MonoBehaviour
+{
+    public float health;
+    public Image healthImage;
+
+    void Update()
+    {
+        healthImage.fillAmount = (health / 100);
+    }
+}
