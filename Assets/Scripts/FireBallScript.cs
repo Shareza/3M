67a3e82745b3dcf9676ad29b9775d87d9200dd8d
Shareza <michal.soja90@gmail.com>
2017-11-12 18:21:16 +0100

    Added Fireball damage

--- Assets/Scripts/FireBallScript.cs
+++ Assets/Scripts/FireBallScript.cs
@@ -8,12 +8,16 @@ public class FireBallScript : MonoBehaviour {
 
     Collision collision;
     Rigidbody rb;
+    DamageManager damageManager = new DamageManager();
 
     public GameObject explosion;
+    public float minDamage = 10;
+    public float maxDamage = 25;
+    public float power = 10;
     public float speed = 1.5f;
     public float lifeTime = 1.0f;
-    
 
+   
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -33,22 +37,20 @@ public class FireBallScript : MonoBehaviour {
 
     private void OnCollisionEnter(Collision other)
     {
-
+        
 
         if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Shootable")
         {
             explosion = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
             Destroy(gameObject);
         }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
         DealDamage(other);
     }
 
-    public void DealDamage(Collider other)
+    public void DealDamage(Collision other)
     {
-        //other.gameObject.GetComponent<>()
+        float damage = damageManager.GenerateDamage(power, minDamage, maxDamage);
+
+        other.gameObject.GetComponent<SkeletonHealthManager>().TakeDamage(damage);
     }
 }
---
+++ Assets/Scripts/SkeletonHealthManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkeletonHealthManager : MonoBehaviour
+{
+
+
+    public float health = 1000;
+    public Image healthImage;
+
+
+    void Update()
+    {
+        healthImage.fillAmount = (health / 1000);
+
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+}
